3bb24d6e839be71ada8a41f782e83d227ac86f34
Tomasz Rusin <tomek@rusin.info>
2019-07-10 19:04:04 +0200

    Init project & create actions

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux';
+import countriesRecuder from './countries-reducer';
+
+const reducers = combineReducers ({
+    countriesRecuder
+});
+
+export default reducers;
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+
+import { createStore } from 'redux';
+import reducers from '../reducers/index';
+import { getCountries } from '../actions/actions-countries';
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch(getCountries())
+
+export default store
