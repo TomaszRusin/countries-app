61757371366298f5adcfbbc74ff9c54969a0e4b2
Tomasz Rusin <tomek@rusin.info>
2019-07-11 12:01:43 +0200

    Deleted unwanted dispatch

---
+++ src/presentational/flag-list.component.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Link } from 'react-router';
+import CountryFlag from './flag.component';
+
+const CountryFlagList = (props) => (
+    <div className="countries-list">
+        {props.countries.map(country => {
+            return (
+                <div className="single-country" key={country.id}>
+                    <Link className='logo' to={'countries/country/' + country.id}>
+                        <CountryFlag country={country} />
+                    </Link>
+                </div>
+            )
+        })}
+    </div>
+);
+
+export default CountryFlagList;
--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,4 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-store.dispatch(getCountries())
-
 export default store
